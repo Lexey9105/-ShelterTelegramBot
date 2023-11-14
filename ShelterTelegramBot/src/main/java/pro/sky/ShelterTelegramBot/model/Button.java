8e1f66b8b13692ea134883a383221ca383b420b4
Alsahir <sudarkin.jan@yandex.ru>
2023-11-14 22:46:22 +0700

    Добавлен меню с выбором питомника

--- ShelterTelegramBot/src/main/java/pro/sky/ShelterTelegramBot/model/Button.java
+++ ShelterTelegramBot/src/main/java/pro/sky/ShelterTelegramBot/model/Button.java
@@ -1,4 +1,22 @@
 package pro.sky.ShelterTelegramBot.model;
 
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+
 public class Button {
+
+    public InlineKeyboardMarkup animalSelectionButtons() {
+
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+
+        InlineKeyboardButton catButton = new InlineKeyboardButton("Приют кошек").
+                callbackData("CAT_SHELTER_CALLBACK");
+        InlineKeyboardButton dogButton = new InlineKeyboardButton("Приют собак").
+                callbackData("DOG_SHELTER_CALLBACK");
+
+        markupInline.addRow(catButton);
+        markupInline.addRow(dogButton);
+
+        return markupInline;
+    }
 }
